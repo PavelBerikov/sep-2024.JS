72f0c4f192b63c98678f7b00af68f82643f3e18f
PavelBerikov <NoooB199505@gmail.com>
2024-11-21 23:18:22 +0200

    8hw

---
+++ lesson8-HW/main.js
@@ -0,0 +1,55 @@
+/*#AiN5CoUQ
+Створити функцію, яка робить глибоку копію об'єкту.
+Додати перевірки на undefined, null, NaN.
+Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
+
+*/
+const user = {
+    name: 'Vasya',
+    age: 21,
+    status: true,
+    greeting(){
+        console.log(`hello ${this.name}`)
+    },
+    wife: {
+        name: 'Anna',
+        age: 22,
+        wifeGreet(){
+            console.log(`hello ${this.name}`)
+        },
+        son: {
+            name: 'misha',
+            sonGreet(){
+                console.log(`hello ${this.name}`)
+            }
+        }
+    }
+}
+let fun = function (obj) {
+    /*Наличие объекта*/
+    if (obj){
+        /*coздаем джсн копию*/
+        const copy = JSON.parse(JSON.stringify(obj))
+        /*достаем ключ*/
+        for (const objKey in obj) {
+            let item = obj[objKey]
+            /*если значение ключа является фн */
+            if (typeof item === 'function'){
+            /*то добавляем поле в копию объекта */
+                copy[objKey] = item
+            }else if (typeof obj[objKey] === 'object'){
+                /*copy[objKey] = fun(item)*/
+                copy[objKey] = item
+            }
+        }
+        return copy
+    }else {
+        console.log('else')
+    }
+}
+
+let copy = fun(user)
+copy.greeting()
+copy.wife.wifeGreet()
+copy.wife.son.sonGreet()
+console.log(copy)
