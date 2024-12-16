9fa6491658461d99cdc49f5890d6c734b8ecbac7
pavlosidukov <NoooB199505@gmail.com>
2024-12-16 11:31:09 +0200

    *

--- lesson10-HW/last.js
+++ lesson10-HW/last.js
@@ -8,20 +8,59 @@ while (i < 100) {
     users.push({id: users.length + 1, name: 'Vasya' + (users.length + 1)})
     i++
 }
-console.log(users)
-// document.createElement("div")
-const foo = (value) => {
-    for (const user of users) {
-        if (user.id <= value) {
-            document.write(`<div>${JSON.stringify(user)}</div>`)
-        }
-    }
+
+
+if (!localStorage.getItem('count1') && !localStorage.getItem('count2')){
+    localStorage.setItem('count1', '1')
+    localStorage.setItem('count2', '10')
 }
-document.write(`<div>`)
-foo(10)
-document.write(`</div>`)
+
+const divElement = document.createElement('div');
+document.body.appendChild(divElement)
 const prev = document.createElement("button")
 const next = document.createElement("button")
 prev.innerText = 'prev'
 next.innerText = 'next'
+
+
 document.body.append(prev, next)
+const foo = () => {
+    const count1 = localStorage.getItem('count1');
+    const count2 = localStorage.getItem('count2');
+    for (const user of users) {
+        if (user.id >= count1 && user.id <= count2) {
+            const div = document.createElement('div');
+            div.innerText = `${user.id} - ${user.name}`
+            divElement.append(div)
+        }
+    }
+    prev.disabled = parseInt(localStorage.getItem('count1')) <= 1;
+    next.disabled = parseInt(localStorage.getItem('count2')) >= users.length;
+}
+foo()
+
+
+next.addEventListener('click', function (eu) {
+    eu.preventDefault()
+    const count1 = localStorage.getItem('count1');
+    const count2 = localStorage.getItem('count2');
+    localStorage.setItem('count1', parseInt(count1) + 10 + '')
+    localStorage.setItem('count2', parseInt(count2) + 10 + '')
+    divElement.innerText = ''
+    foo()
+
+
+
+})
+
+prev.addEventListener('click', function (eu) {
+    eu.preventDefault()
+    const count1 = localStorage.getItem('count1');
+    const count2 = localStorage.getItem('count2');
+    localStorage.setItem('count1', parseInt(count1) - 10 + '')
+    localStorage.setItem('count2', parseInt(count2) - 10 + '')
+    divElement.innerText = ''
+    foo()
+
+
+})
